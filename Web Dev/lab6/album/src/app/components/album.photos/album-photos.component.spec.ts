f20195c3f530168f63de9688e3c2efffc39d063e
MirasTemirov <115141212+MirasTemirov@users.noreply.github.com>
2023-03-08 23:03:17 +0600

    Create album-photos.component.spec.ts

---
+++ Web Dev/lab6/album/src/app/components/album.photos/album-photos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlbumPhotosComponent } from './album-photos.component';
+
+describe('AlbumPhotosComponent', () => {
+  let component: AlbumPhotosComponent;
+  let fixture: ComponentFixture<AlbumPhotosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AlbumPhotosComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AlbumPhotosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
