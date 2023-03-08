61cec3b41875b95a4cb37e2ac2352e8c913cf93e
MirasTemirov <115141212+MirasTemirov@users.noreply.github.com>
2023-03-08 22:53:55 +0600

    Create app-routing.module.ts

---
+++ Web Dev/lab6/album/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {AlbumsComponent} from "./components/albums/albums.component";
+import {AlbumDetailComponent} from "./components/album-detail/album-detail.component";
+import {AlbumPhotosComponent} from "./components/album-photos/album-photos.component";
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'albums', component: AlbumsComponent},
+  {path: 'albums/:id', component: AlbumDetailComponent},
+  {path: 'albums/:id/photos', component: AlbumPhotosComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
