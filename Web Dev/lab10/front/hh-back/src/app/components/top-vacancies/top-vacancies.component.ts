753a09f7f95b3a273335ea01e4236b018c3c2fe6
MirasTemirov <115141212+MirasTemirov@users.noreply.github.com>
2023-04-19 15:21:12 +0300

    Create top-vacancies.component.ts

---
+++ Web Dev/lab10/front/hh-back/src/app/components/top-vacancies/top-vacancies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-vacancies',
+  templateUrl: './top-vacancies.component.html',
+  styleUrls: ['./top-vacancies.component.css']
+})
+export class TopVacanciesComponent {
+
+}
